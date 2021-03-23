fbd723289fcab507bd1b9816307b300d32ad5c00
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-03-23 16:07:42 -0400

    created route for elos

--- NBA-Predictions/Web/app.py
+++ NBA-Predictions/Web/app.py
@@ -29,6 +29,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 Model = Base.classes.model
+Elos = Base.classes.elos
 
 # HTML Routes
 @app.route("/")
@@ -46,9 +47,49 @@ def teamsroute():
 @app.route("/elo")
 def eloroute():
     session = Session(engine)
+    sel = [Elos.date, Elos.bucks, Elos.raptors, Elos.sixers,
+    Elos.celtics, Elos.pacers, Elos.nets, Elos.magic, Elos.pistons, Elos.hornets,	
+    Elos.heat,	Elos.wizards, Elos.hawks, Elos.bulls, Elos.cavs,	
+    Elos.knicks, Elos.warriors, Elos.nuggets, Elos.rockets, Elos.blazers, Elos.jazz, Elos.thunder, Elos.clippers,
+    Elos.spurs, Elos.kings, Elos.lakers, Elos.timberwolves, Elos.mavericks, Elos.grizzlies, Elos.pelicans, Elos.suns]
+    results = session.query(*sel).all()
     session.close()
-    elo_df = {}
-    return jsonify(elo_df)
+    elo_list = []
+    for result in results:
+        elo_dict ={}
+        elo_dict["Date"] = result[0]
+        elo_dict["Milwaukee Bucks"] = result[1]
+        elo_dict["Toronto Raptors"] = result[2]
+        elo_dict["Philadelphia 76ers"] = result[3]
+        elo_dict["Boston Celtics"] = result[4]
+        elo_dict["Indiana Pacers"] = result[5]
+        elo_dict["Brooklyn Nets"] = result[6]
+        elo_dict["Orlando Magic"] = result[7]
+        elo_dict["Detroit Pistons"] = result[8]
+        elo_dict["Charlotte Hornets"] = result[9]
+        elo_dict["Miami Heat"] = result[10]
+        elo_dict["Washington Wizards"] = result[11]
+        elo_dict["Atlanta Hawks"] = result[12]
+        elo_dict["Chicago Bulls"] = result[13]
+        elo_dict["Cleveland Cavaliers"] = result[14]
+        elo_dict["New York Knicks"] = result[15]
+        elo_dict["Golden State Warriors"] = result[16]
+        elo_dict["Denver Nuggets"] = result[17]
+        elo_dict["Houston Rockets"] = result[18]
+        elo_dict["Portland Trail Blazers"] = result[19]
+        elo_dict["Utah Jazz"] = result[20]
+        elo_dict["Oklahoma City Thunder"] = result[21]
+        elo_dict["Los Angeles Clippers"] = result[22]
+        elo_dict["San Antonio Spurs"] = result[23]
+        elo_dict["Sacramento Kings"] = result[24]
+        elo_dict["Los Angeles Lakers"] = result[25]
+        elo_dict["Minnesota Timberwolves"] = result[26]
+        elo_dict["Dallas Mavericks"] = result[27]
+        elo_dict["Memphis Grizzlies"] = result[28]
+        elo_dict["New Orleans Pelicans"] = result[29]
+        elo_dict["Phoenix Suns"] = result[30]
+        elo_list.append(elo_dict)
+    return jsonify(elo_list)
 
 @app.route("/model")
 def modelroute():
